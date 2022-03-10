27db496674882e1a0f4475ecd9f5776d9c50c751
Vinit Harsora <harsora.v@northeastern.edu>
2022-03-10 10:17:17 -0500

    Initial Commit

--- Controller/usersController.js
+++ Controller/usersController.js
@@ -1,25 +1,31 @@
 const db = require('../config/sequelizeDB.js');
 const User = db.users;
 const bcrypt = require('bcrypt');
-const {v4:uuidv4} = require('uuid');
+const {
+    v4: uuidv4
+} = require('uuid');
 
 
 // Create a User
 
-async function createUser (req, res, next) {
+async function createUser(req, res, next) {
     var hash = await bcrypt.hash(req.body.password, 10);
     const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-    if(!emailRegex.test(req.body.username)) {
+    if (!emailRegex.test(req.body.username)) {
         res.status(400).send({
             message: 'Enter your Email ID in correct format. Example: abc@xyz.com'
         });
     }
-    const getUser = await User .findOne({where: {username: req.body.username}}).catch(err => {
+    const getUser = await User.findOne({
+        where: {
+            username: req.body.username
+        }
+    }).catch(err => {
         res.status(500).send({
             message: err.message || 'Some error occurred while creating the user'
         });
     });
-    if(getUser) {
+    if (getUser) {
         res.status(400).send({
             message: 'User already exists!'
         });
@@ -31,23 +37,22 @@ async function createUser (req, res, next) {
             password: hash,
             username: req.body.username
         };
-    
-    User.create(user).then(data => {
-        res.status(201).send({
-            id: data.id,
-            first_name: data.first_name,
-            last_name: data.last_name,
-            username: data.username,
-            account_created: data.createdAt,
-            account_updated: data.updatedAt
-        });
-    })
-    .catch(err => {
-        res.status(500).send({
-            message:
-                err.message || "Some error occurred while creating the user!"
-        });
-    });
+
+        User.create(user).then(data => {
+                res.status(201).send({
+                    id: data.id,
+                    first_name: data.first_name,
+                    last_name: data.last_name,
+                    username: data.username,
+                    account_created: data.createdAt,
+                    account_updated: data.updatedAt
+                });
+            })
+            .catch(err => {
+                res.status(500).send({
+                    message: err.message || "Some error occurred while creating the user!"
+                });
+            });
     }
 }
 
@@ -74,10 +79,10 @@ async function getUser(req, res, next) {
 // Update a user
 
 async function updateUser(req, res, next) {
-    if(req.body.username != req.user.username) {
+    if (req.body.username != req.user.username) {
         res.status(400);
     }
-    if(!req.body.first_name || !req.body.last_name || !req.body.username || !req.body.password) {
+    if (!req.body.first_name || !req.body.last_name || !req.body.username || !req.body.password) {
         res.status(400).send({
             message: 'Enter all parameters!'
         });
@@ -86,12 +91,16 @@ async function updateUser(req, res, next) {
         first_name: req.body.first_name,
         last_name: req.body.last_name,
         password: await bcrypt.hash(req.body.password, 10)
-    }, {where : {username: req.user.username}}).then((result) => {
+    }, {
+        where: {
+            username: req.user.username
+        }
+    }).then((result) => {
         if (result == 1) {
             res.sendStatus(204);
         } else {
             res.sendStatus(400);
-        }   
+        }
     }).catch(err => {
         res.status(500).send({
             message: 'Error Updating the user'
@@ -100,10 +109,15 @@ async function updateUser(req, res, next) {
 }
 
 async function getUserByUsername(username) {
-    return User.findOne({where : {username: username}});
+    
+    return User.findOne({
+        where: {
+            username: username
+        }
+    });
 }
 
-async function comparePasswords (existingPassword, currentPassword) {
+async function comparePasswords(existingPassword, currentPassword) {
     return bcrypt.compare(existingPassword, currentPassword);
 }
 
--- packer/provisioners.sh
+++ packer/provisioners.sh
@@ -3,13 +3,18 @@
 sleep 30
 
 sudo yum update -y
-
+sleep 10
 sudo yum install git make gcc -y
+sleep 10
 sudo amazon-linux-extras install epel
+sleep 10
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash ~/.nvm/nvm.sh
 sudo yum install -y gcc-c++ make
+sleep 10
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
 sudo yum install -y nodejs
+sleep 10
 sudo npm install -g pm2
+sleep 10
 mkdir /home/ec2-user/node-app
 chown ec2-user:ec2-user /home/ec2-user/node-app
